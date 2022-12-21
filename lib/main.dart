98183764c74f304b5e8f063c589bba54b6ea6cf7
J05K4 <diazmoranjosue@gmail.com>
2022-12-21 07:46:00 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_5/pages/PaginaHome.dart';
+import 'package:flutter_application_5/pages/PaginaUsers.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _paginaActual = 1;
+
+  List<Widget> _paginas = [
+    PaginaHome(),
+    PaginaUsers(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: _paginas[_paginaActual],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _paginaActual = index;
+            });
+          },
+          currentIndex: _paginaActual,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.supervised_user_circle), label: "Users")
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/PaginaUsers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PaginaUsers extends StatelessWidget {
+  const PaginaUsers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Users"),
+    );
+  }
+}
